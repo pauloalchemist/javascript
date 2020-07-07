b71dcd6b80c01a94c51160f68bf2b19436562e5e
pauloalchemist <paulolinsdev@gmail.com>
2020-07-06 22:34:21 -0300

    refatoração e inclusão de remover, listar e cadastrar.

--- apis_node/cli/database.js
+++ apis_node/cli/database.js
@@ -1,23 +1,52 @@
-const { readFile } = require('fs')
-const { promisify } = require('util')
-const readFileAsync = promisify(readFile)
+const { readFile, writeFile } = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
+
 class Database {
-    constructor(){
-        this.NOME_ARQUIVO = 'herois.json'
-    }
-    async obterDados() {
-        const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
-        return JSON.parse(arquivo.toString())
-    }
-    escreveDados() {
+	constructor() {
+		this.NOME_ARQUIVO = 'herois.json';
+	};
+	async obterArquivo() {
+		const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8');
+		return JSON.parse(arquivo.toString());
+	};
+	async escreverArquivo(dados) {
+		await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados));
+		return true;
+	};
+	async cadastrar(heroi) {
+		const dados =await this.obterArquivo();
+		const id = heroi.id <= 2 ? heroi.id : Date.now();
+		const heroiComId = {
+			id,
+			...heroi
+		};
+		const dadosFinal = [
+			...dados,
+			heroiComId
+		];
+		const resultado = await this.escreverArquivo(dadosFinal);
 
-    }
-    async listar(id) {
-        const dados =await this.obterDados()
-        const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true))
-        return dadosFiltrados        
-    }
-}
+		return resultado;
+	};
+	async listar(id) {
+		const dados = await this.obterArquivo();
+		const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true));
+		return dadosFiltrados;
+	};
+	async remover(id) {
+		if(!id) {
+			return await this.escreverArquivo([]);
+		};
+		const dados = await this.obterArquivo();
+		const indice = dados.findIndex(item => item.id === parseInt(id));
+		if(indice === -1) {
+			throw Error('Heroi não existe.');
+		};
+		dados.splice(indice, 1);
+		return await this.escreverArquivo(dados);
+	};
+};
 
-
-module.exports = new Database()
+module.exports = new Database();

3b631a7ce730a2fc5c419445ef4df8dcef9ad7e9
pauloalchemist <paulolinsdev@gmail.com>
2020-07-14 00:28:10 -0300

    removido [] e ... para melhorar código de acesso as rotas

--- api_estrutura/src/api.js
+++ api_estrutura/src/api.js
@@ -4,9 +4,12 @@ const Context = require('./db/strategies/base/contextStrategy');
 const MongoDb = require('./db/strategies/mongodb/mongodb');  
 const HeroiSchema = require('./db/strategies/mongodb/schemas/heroisSchema');
 const HeroRoutes = require('./routes/heroRoutes');
+const HapiSwagger = require('hapi-swagger');
+const Vision = require('@hapi/vision');
+const Inert = require('@hapi/inert');
 
 const app = new hapi.Server({
-    port: 7070
+    port: 5000
 });
 
 function mapRoutes(instance, methods) {
@@ -16,10 +19,26 @@ function mapRoutes(instance, methods) {
 async function main() {
     const connection = MongoDb.connect(); 
     const context = new Context(new MongoDb(connection, HeroiSchema));
-    app.validator(Joi);
-    app.route([
-        ...mapRoutes(new HeroRoutes(context), HeroRoutes.methods())
+    const swaggerOptions = {
+        info: {
+            title: 'API Herois - Curso NodeBR',
+            version: 'v1.0'
+        },
+      
+    };
+    await app.register([
+        Vision,
+        Inert,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
     ]);
+
+    app.validator(Joi);
+    app.route(
+        mapRoutes(new HeroRoutes(context), HeroRoutes.methods())
+    );
 
     await app.start();
     console.log('Servidor rodando na porta:', app.info.port);

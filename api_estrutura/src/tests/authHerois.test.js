972b73e1b4d0336694fbbb68e65dde8b63a7c0a4
pauloalchemist <paulolinsdev@gmail.com>
2020-07-14 18:22:13 -0300

    criaão de teste para autorização com JWT

---
+++ api_estrutura/src/tests/authHerois.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const api = require('../api');
+let app = {};
+
+describe('Auth test suite', function () {
+    this.beforeAll(async () => {
+        app = await api;
+    });
+
+    it('Deve obter um token', async () => {
+        const result = await app.inject({
+            method: 'POST',
+            url: '/login',
+            payload: {
+                username: 'platão',
+                password: '12345'
+            }
+        });
+        const statusCode = result.statusCode;
+        const dados = JSON.parse(result.payload);
+        assert.deepEqual(statusCode, 200);
+        assert.ok(dados.token.length > 10);
+    });
+});

4d3a04ed1bb9e20148bfae07866f088dfbcd2ea0
pauloalchemist <paulolinsdev@gmail.com>
2020-07-15 16:13:09 -0300

    refatoração com JWT

--- api_estrutura/src/tests/authHerois.test.js
+++ api_estrutura/src/tests/authHerois.test.js
@@ -1,24 +1,53 @@
 const assert = require('assert');
 const api = require('../api');
+const Context = require('../db/strategies/base/contextStrategy');
+const Postgres = require('../db/strategies/postgres/postgres');
+const UsuarioSchema = require('../db/strategies/postgres/schemas/usuarioSchema');
 let app = {};
+
+const USER = {
+     username: 'platão',
+     password: '123'
+};
+
+const USER_DB = {
+    username: USER.username.toLowerCase(),
+    password: '$2b$04$SSJ6ketOHQrXreC.YxPF7.ScZZOBh9M28W0QvlzJjcw5AdhI.RE.u'
+};
 
 describe('Auth test suite', function () {
     this.beforeAll(async () => {
         app = await api;
+        const connectionPostgres = await Postgres.connect();
+        const model = await Postgres.defineModel(connectionPostgres, UsuarioSchema);
+        const postgres = new Context(new Postgres(connectionPostgres, model));
+        await postgres.update(null, USER_DB, true);
     });
 
     it('Deve obter um token', async () => {
         const result = await app.inject({
             method: 'POST',
             url: '/login',
-            payload: {
-                username: 'platão',
-                password: '12345'
-            }
+            payload: USER
         });
         const statusCode = result.statusCode;
         const dados = JSON.parse(result.payload);
         assert.deepEqual(statusCode, 200);
         assert.ok(dados.token.length > 10);
     });
+    it('deve retornar não autorizado com login errado', async () => {
+        const result = await app.inject({
+            method: 'POST',
+            url: '/login',
+            payload: {
+                username: 'paulolins',
+                password: '123'
+            }
+        });
+        const statusCode = result.statusCode;
+        const dados = JSON.parse(result.payload);
+        assert.deepEqual(statusCode, 401);
+        assert.deepEqual(dados.error, 'Unauthorized');
+    });
+    
 });

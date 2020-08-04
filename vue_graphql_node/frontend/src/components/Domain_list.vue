787c19afc1209b7afef1de0abaabd8e1f39e4e2a
pauloalchemist <paulolinsdev@gmail.com>
2020-08-04 16:49:32 -0300

    vue+node+graphql

---
+++ vue_graphql_node/frontend/src/components/Domain_list.vue
@@ -0,0 +1,143 @@
+<template>
+      <div id="main">
+        <div class="container">
+          <div class="row">
+            <div class="col-md">
+							<AppItemList title="Prefixo" v-bind:items="prefixes" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></AppItemList>
+            </div>
+            <div class="col-md">
+							<AppItemList title="Sufixo" v-bind:items="sufixes" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></AppItemList>
+            </div>
+        </div>
+        <br/>
+        <h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+								<div class="row">
+									<div class="col-md">{{ domain.name }}</div>
+									<div class="col md text-right">
+										<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+											<span class="fa fa-shopping-cart"></span>
+										</a>
+									</div>
+								</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      </div>
+    
+  
+</template>
+
+<script>
+import axios from "axios/dist/axios";
+import AppItemList from "./AppItemList";
+
+export default {
+	name: "App",
+	components: {
+		AppItemList
+	},
+	data: function () {
+		return {
+			prefixes: [],
+			sufixes: [],
+		};
+	},
+	methods: {
+		addPrefix(prefix) {
+			//this.prefixes.push(prefix);
+			axios({
+				url: "http://localhost:4000",
+				method: "post",
+				data: {
+					query: `
+						mutation ($item: ItemInput) {
+							newPrefix: saveItem(item: $item) {
+								id
+								type
+								description
+							}
+						}
+					`,
+					variables: {
+						item: {
+							type: "prefix",
+							description: prefix
+						}
+					}
+				}
+			});
+		},
+		deletePrefix(prefix) {
+			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+		},
+		addSufix(sufix) {
+			this.sufixes.push(sufix);
+		},
+		deleteSufix(sufix) {
+			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+		}
+	},
+	computed: {
+		domains() {
+			const domains = [];
+			for (const prefix of this.prefixes) {
+				for (const sufix of this.sufixes) {
+					const name = prefix + sufix;
+					const url = name.toLowerCase();
+					const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+					domains.push({
+						name,
+						checkout
+					});
+				}
+			}
+			return domains;
+		}
+	},
+	created() {
+		axios({
+			url: "http://localhost:4000",
+			method: "post",
+			data: {
+				query:`
+					{
+						prefixes: items (type: "prefix") {
+							id 
+							type
+							description
+						}
+						sufixes: items (type: "sufix") {
+							description
+						}
+						
+					}
+				`
+			}
+		}).then(response => {
+			const query = response.data;
+			this.prefixes = query.data.prefixes.map(prefix => prefix.description);
+			this.sufixes = query.data.sufixes.map(sufix => sufix.description);
+		});
+	}
+};
+</script>
+
+<style>
+  #slogan {
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
+
+  #main {
+    background-color: #F1F1F1;
+    padding-top: 30px;
+    padding-bottom: 30px;
+
+  }
+</style>

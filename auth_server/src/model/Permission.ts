8d9c61170195c50880677350b3f738ccf62602c2
pauloalchemist <paulolinsdev@gmail.com>
2020-08-21 17:56:13 -0300

    model Permission

---
+++ auth_server/src/model/Permission.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+
+@Entity('permission')
+class Permission {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+  @Column()
+  name: string;
+  @Column()
+  description: string;
+  @CreateDateColumn()
+  created_at: Date;
+}
+
+export default Permission;

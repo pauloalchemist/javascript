22d5f0c43e57cde981fad8794d9deb3ec3359f43
pauloalchemist <paulolinsdev@gmail.com>
2020-09-03 22:25:04 -0300

    User model

---
+++ tdd_jest_auth/src/app/models/User.js
@@ -0,0 +1,31 @@
+const bccryptt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.VIRTUAL,
+    password_hash: DataTypes.STRING
+  },
+    {
+      hooks: {
+        beforeSave: async user => {
+          if (user.password) {
+            user.password_hash = await bccryptt.hash(user.password, 8)
+          }
+        }
+      }
+    }
+  );
+
+  User.prototype.checkPassword = function(password) {
+    return bccryptt.compare(password, this.password_hash)
+  };
+
+  User.prototype.generateToken = function() {
+    return jwt.sign({ id: this.id}, process.env.APP_SECRET);
+  }
+
+  return User;
+};

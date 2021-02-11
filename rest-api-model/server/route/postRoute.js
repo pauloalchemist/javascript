cc572486350e047a773b0435eca2c34d6e491807
pauloalchemist <paulolinsdev@gmail.com>
2021-02-11 12:21:23 -0300

    .lintstagedrc.json

--- rest-api-model/server/route/postRoute.js
+++ rest-api-model/server/route/postRoute.js
@@ -2,5 +2,8 @@ const router = require('express').Router()
 
 router.get('/posts')
 router.get('/posts/:id')
+router.post('/posts')
+router.put('/posts/:id')
+router.delete('posts/:id')
 
 module.exports = router

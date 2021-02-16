315b620280a049555330a3bc3344d207eb3d064f
pauloalchemist <paulolinsdev@gmail.com>
2021-02-16 00:34:50 -0300

    module.exports

--- rest-api-model/server/data/postData.js
+++ rest-api-model/server/data/postData.js
@@ -1,25 +1,27 @@
 const database = require('../infra/connectionDb')
 
-exports.getPosts = () => {
-  return database.query('select * from blog.post')
-}
+module.exports = {
+  getPosts () {
+    return database.query('select * from blog.post')
+  },
 
-exports.getPost = (id) => {
-  return database.oneOrNone('select * from blog.post where id = $1', [id])
-}
+  getPost (id) {
+    return database.oneOrNone('select * from blog.post where id = $1', [id])
+  },
 
-exports.savePost = (post) => {
-  return database.one(
-    'insert into blog.post (title, content) values ($1, $2) returning *', [post.title, post.content]
-  )
-}
+  savePost (post) {
+    return database.one(
+      'insert into blog.post (title, content) values ($1, $2) returning *', [post.title, post.content]
+    )
+  },
 
-exports.updatePost = (id, post) => {
-  return database.none(
-    'update blog.post set title = $1, content = $2 where id = $3', [post.title, post.content, id]
-  )
-}
+  updatePost (id, post) {
+    return database.none(
+      'update blog.post set title = $1, content = $2 where id = $3', [post.title, post.content, id]
+    )
+  },
 
-exports.deletePost = (id) => {
-  return database.none('delete from blog.post where id = $1', [id])
+  deletePost (id) {
+    return database.none('delete from blog.post where id = $1', [id])
+  }
 }

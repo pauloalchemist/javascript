7243bdc381a1fb5343889aca730f8ab9d42538e7
pauloalchemist <paulolinsdev@gmail.com>
2021-04-10 13:37:32 -0300

    concat()

--- stringMethods.js
+++ stringMethods.js
@@ -3,5 +3,8 @@ let stringTwo = 'JavaScript é uma linguagem legal que evolui a cada dia.'
 
 // chartAt() localiza caracter conforme indice infromado 
 console.log(`O caracter com indíce 4 é o ${stringOne.charAt(4)}.`)
+// concat() concatena strings e pode inserir novos elementos.
+console.log(stringOne.concat(stringTwo))
+console.log(stringOne.concat(' ', stringTwo))
 
 

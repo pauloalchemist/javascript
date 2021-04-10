ae09a004f351f94c586ebf33de551a8dddd44c89
pauloalchemist <paulolinsdev@gmail.com>
2021-04-10 13:31:44 -0300

    charAt()

---
+++ stringMethods.js
@@ -0,0 +1,7 @@
+let stringOne = 'Estes são alguns exemplos de métodos com strings wm JavaScript.'
+let stringTwo = 'JavaScript é uma linguagem legal que evolui a cada dia.'
+
+// chartAt() localiza caracter conforme indice infromado 
+console.log(`O caracter com indíce 4 é o ${stringOne.charAt(4)}.`)
+
+

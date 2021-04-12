7dcd5cda4983f5768a6a7d274bddea17076728cf
pauloalchemist <paulolinsdev@gmail.com>
2021-04-12 13:44:50 -0300

    refactoring reduce

--- reduce-exemple.js
+++ reduce-exemple.js
@@ -1,11 +1,10 @@
 const frutas = ['maçã', 'maçã', 'banana', 'laranja', 'uva', 'abacaxi', 'acerola']
 
 const contagem = frutas.reduce((acc, fruta) => {
-  if (acc[fruta]) {
-    acc[fruta] = acc[fruta] + 1
-  } else {
-    acc[fruta] = 1
-  }
+  const frutaCount = acc[fruta]
+  frutaCount
+    ? acc[fruta] = frutaCount + 1 
+    : acc[fruta] = 1 
   return acc
 }, {})
 

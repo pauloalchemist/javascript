7b7366275ecc382f36b076e7549ba2ef1be09dfe
pauloalchemist <paulolinsdev@gmail.com>
2021-04-19 12:08:18 -0300

    rota get

--- typescript-project/src/server.ts
+++ typescript-project/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 const app = express()
 
+app.get('/', (req, res) => {
+  res.json({message: "tá rodando!"})
+})
+
 app.listen(3333, () => {
   console.log('Server ativo na porta 3333')
 })
